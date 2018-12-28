cfc34f38145c80d2d92d7992158cefd3325ee8bb
heying1207abc <heying1207@126.com>
2018-12-28 16:52:10 +0800

    Revert "Initial commit"
    This reverts commit ae5c8daa19337ad3fffeee6c629d2e4ceafd2e6f.

--- src/com/heying/hrmanager/mapper/AttendanceflowingCustomMapper.java
+++
@@ -1,8 +0,0 @@
-package com.heying.hrmanager.mapper;
-
-public interface AttendanceflowingCustomMapper {
-	//通过考勤流水号得到需修改的考勤状态
-	public int findStateByBeginTime(Integer id);
-	//通过上班打卡时间及员工id得到当考勤流水号
-	public Integer findIdByBeginTime(Integer employeeId);
-}
